1b36e2d54e43f9ef832dc08d028dbcc9c0e1a0ed
ledaithanh1006-netizen <ledaithanh1006@gmail.com>
2026-09-23 16:20:25 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,13 @@
-const CACHE = 'thai-tri-co-v1';
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { self.clients.claim(); });
+const CACHE = 'thai-tri-co-v2';
+const SHELL = ['/', '/index.html'];
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL).catch(() => {})));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   const url = new URL(e.request.url);
